7fb3e92e37ab1ed5bee9ced204163d1a9d293fac
Julian Friedman <julz.friedman@uk.ibm.com>
2014-09-04 11:08:43 +0100

    Add detected_start_command to staging response
    [#76096574]
    
    Signed-off-by: Matthew Sykes <matthew.sykes@gmail.com>
    Signed-off-by: Michael Fraenkel <fraenkel@us.ibm.com>

--- spec/unit/responders/staging_spec.rb
+++ spec/unit/responders/staging_spec.rb
@@ -23,6 +23,7 @@ describe Dea::Responders::Staging do
       task_log: "task-log",
       detected_buildpack: nil,
       buildpack_key: buildpack_key,
+      detected_start_command: "bacofoil",
       droplet_sha1: "some-droplet-sha",
       memory_limit_mb: 1,
       disk_limit_mb: 2,
@@ -258,7 +259,8 @@ describe Dea::Responders::Staging do
               "task_id" => "task-id",
               "detected_buildpack" => nil,
               "buildpack_key" => "some_buildpack_key",
-              "droplet_sha1" => "some-droplet-sha"
+              "droplet_sha1" => "some-droplet-sha",
+              "detected_start_command" => "bacofoil",
             ))
             subject.handle(message)
           end
@@ -313,6 +315,7 @@ describe Dea::Responders::Staging do
               "detected_buildpack" => nil,
               "buildpack_key" => nil,
               "droplet_sha1" => nil,
+              "detected_start_command" => "bacofoil",
               "error" => "error-description",
               "error_info" => staging_error_info,
             ))
